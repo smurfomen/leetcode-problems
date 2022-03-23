e3579997f0e9fb1e13311803b4d540b155177be1
pcuser <vova.skiller@gmail.com>
2022-03-23 13:47:49 +0300

    [MEDIUM] Problem solved: 33. Search in Rotated Sorted Array

---
+++ 33-search-in-rotated-array.c
@@ -0,0 +1,84 @@
+#include <string>
+#include <cassert>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+    There is an integer array nums sorted in ascending order (with distinct values).
+    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Example 1:
+        Input: nums = [4,5,6,7,0,1,2], target = 0
+        Output: 4
+
+    Example 2:
+        Input: nums = [4,5,6,7,0,1,2], target = 3
+        Output: -1
+
+    Example 3:
+        Input: nums = [1], target = 0
+        Output: -1
+
+
+    Constraints:
+        1 <= nums.length <= 5000
+        -10^4 <= nums[i] <= 10^4
+        All values of nums are unique.
+        nums is an ascending array that is possibly rotated.
+        -10^4 <= target <= 10^4
+*/
+class Solution {
+public:
+    int search(const vector<int>& nums, int target) {
+        size_t n = nums.size();
+        if(!n)
+            return -1;
+
+        int beg = 0;
+        int end = n-1;
+
+        while(beg <= end)
+        {
+            int mid = (beg+end)/2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[mid] <= nums[end])
+            {
+                if(target >= nums[mid] && target <= nums[end])
+                    beg = mid+1;
+                else
+                    end = mid-1;
+            }
+            else
+            {
+                if(target >= nums[beg] && target <= nums[mid])
+                    end = mid-1;
+                else
+                    beg = mid+1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main(int argc, char * argv[]) {
+    Solution sol;
+
+    assert(sol.search({4,5,6,7,0,1,2}, 0) == 4);
+    assert(sol.search({4,5,6,7,0,1,2}, 3) == -1);
+    assert(sol.search({1}, 0) == -1);
+    assert(sol.search({1}, 1) == 0);
+    assert(sol.search({1,3}, 1) == 0);
+    assert(sol.search({4,6,0,1,3}, 1) == 3);
+
+    cout << "tests is passed!" << endl;
+
+    return 0;
+}
