65516877bd4c33c0c3cc46094d498108ffdb6ea1
pcuser <vova.skiller@gmail.com>
2023-05-31 11:51:17 +0300

    [EASY] Problem solved: 121. Best Time to Buy and Sell Stock.

---
+++ 121-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,64 @@
+#include <cassert>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+/*
+            You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+            You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+            Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+            Example 1:
+                    Input: prices = [7,1,5,3,6,4]
+                    Output: 5
+                    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+                    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+            Example 2:
+                    Input: prices = [7,6,4,3,1]
+                    Output: 0
+                    Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+            Constraints:
+                    1 <= prices.length <= 105
+                    0 <= prices[i] <= 104
+*/
+
+
+
+
+/*
+
+  Time Complexity: O(n)
+
+  Space Complexity: O(1)
+
+*/
+class Solution {
+public:
+    int maxProfit(vector<int> prices) {
+        int result = 0;
+        int min = prices[0];
+
+
+        for(int i = 0; i < prices.size(); ++i) {
+            min = std::min(prices[i], min);
+            result = max(result, prices[i] - min);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    assert(sol.maxProfit({7,1,5,3,6,4}) == 5);
+    assert(sol.maxProfit({7,6,4,3,1}) == 0);
+
+    return 0;
+}
